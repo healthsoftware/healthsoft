87b462538e512f3953c6be5267d044f29782dfee
unknown <jfletes@.(none)>
2013-01-05 11:07:14 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthsoft::Application.config.secret_token = '03cbe1c687fd56fec18c2c0f8ecd8fa7986d6b4a515d0209ea47ff7d102e5c63731bc882feeccd81901e9566309b4fd84a94cb1f63ff214715e1f275cb7dc154'
